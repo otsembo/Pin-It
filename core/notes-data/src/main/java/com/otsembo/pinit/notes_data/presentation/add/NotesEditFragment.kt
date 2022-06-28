83595b6fb00aea74208e533e1d76096a84a4a87e
Ian Okumu <okumu.otsembo@gmail.com>
2022-06-28 12:01:40 +0300

    Notes: Added edit notes fragment

--- core/notes-data/src/main/java/com/otsembo/pinit/notes_data/presentation/add/NotesEditFragment.kt
+++ core/notes-data/src/main/java/com/otsembo/pinit/notes_data/presentation/add/NotesEditFragment.kt
@@ -4,15 +4,17 @@ import android.app.Dialog
 import android.os.Bundle
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
+import com.otsembo.pinit.notes_data.R
 import com.otsembo.pinit.notes_data.databinding.FragmentNoteEditBinding
 
-class NotesEditFragment : DialogFragment(){
+class NotesEditFragment : DialogFragment() {
 
     private lateinit var binding: FragmentNoteEditBinding
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        // val dialogView = LayoutInflater.from(requireActivity()).inflate(R.layout.fragment_note_edit, null, false)
         binding = FragmentNoteEditBinding.inflate(layoutInflater)
-        return AlertDialog.Builder(requireContext())
+        return AlertDialog.Builder(requireContext(), com.otsembo.pinit.theming.R.style.FullScreenDialog)
             .setView(binding.root)
             .create()
     }
--- features/dashboard/src/main/java/com/otsembo/notes/presentation/NotesFragment.kt
+++ features/dashboard/src/main/java/com/otsembo/notes/presentation/NotesFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.otsembo.notes.R
 import com.otsembo.notes.databinding.FragmentDashboardBinding
 
 class NotesFragment : Fragment() {
@@ -13,6 +15,14 @@ class NotesFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        initClicks()
         return binding.root
     }
+
+    private fun initClicks(){
+        binding.fabNotes.setOnClickListener {
+            it.findNavController().navigate(R.id.dashboardToNotesEdit)
+        }
+    }
+
 }
